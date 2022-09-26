913a457b52eaca1c5194b153a2e4a00be9e6be42
Shougo Matsushita <Shougo.Matsu@gmail.com>
2022-09-26 19:52:11 +0900

    lint

--- denops/@ddu-filters/converter_display_word.ts
+++ denops/@ddu-filters/converter_display_word.ts
@@ -8,6 +8,7 @@ import { Denops } from "https://deno.land/x/ddu_vim@v1.11.0/deps.ts";
 type Params = Record<never, never>;
 
 export class Filter extends BaseFilter<Params> {
+  // deno-lint-ignore require-await
   async filter(args: {
     denops: Denops;
     sourceOptions: SourceOptions;
