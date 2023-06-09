18c8989013e11e290d265aaa2b85de647f5bc45a
Shougo Matsushita <Shougo.Matsu@gmail.com>
2023-06-10 07:31:54 +0900

    Update

--- denops/@ddu-filters/converter_display_word.ts
+++ denops/@ddu-filters/converter_display_word.ts
@@ -2,14 +2,13 @@ import {
   BaseFilter,
   DduItem,
   SourceOptions,
-} from "https://deno.land/x/ddu_vim@v2.0.0/types.ts";
-import { Denops } from "https://deno.land/x/ddu_vim@v2.0.0/deps.ts";
+} from "https://deno.land/x/ddu_vim@v3.0.2/types.ts";
+import { Denops } from "https://deno.land/x/ddu_vim@v3.0.2/deps.ts";
 
 type Params = Record<never, never>;
 
 export class Filter extends BaseFilter<Params> {
-  // deno-lint-ignore require-await
-  override async filter(args: {
+  override filter(args: {
     denops: Denops;
     sourceOptions: SourceOptions;
     input: string;
